2e6e2c77347b98507a17e00a9c3785aa3a6b9a28
Bartek Drozdz <bartek@everyday3d.com>
2013-10-21 17:29:04 -0700

    Some updates to demos + a bit of clean-up

--- src/sqr/util/Color.js
+++ src/sqr/util/Color.js
@@ -9,6 +9,9 @@ SQR.Color = function(h, s, l, a) {
 
     this.applyLight = function(dot) {
         // TODO: remove the hardcoded value!
+
+        dot = Math.max(0.2, dot);
+
         return SQR.Color.hsl(this.hue, this.saturation, this.lightness - 60 + 80 * dot, this.alpha);
     }
 
