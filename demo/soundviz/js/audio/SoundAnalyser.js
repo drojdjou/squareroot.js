3a7381e9a0b30e27460ea7821b7551fe000c9dbc
Bartek Drozdz <bartek@everyday3d.com>
2014-02-11 10:43:05 -0800

    Compiled soundviz demo JS

--- demo/soundviz/js/audio/SoundAnalyser.js
+++ demo/soundviz/js/audio/SoundAnalyser.js
@@ -18,8 +18,8 @@ var SoundAnalyser = function() {
 	var timeByteData; // waveform - waveform data is from 0-256 for 512 bins. no sound is 128.
 
 	var beatCutOff = 0, beatTime, beatLevel = 0;
-	var beatHoldTime = 10 ; // num of frames to hold a beat
-	var beatDecayRate = 0.99;
+	var beatHoldTime = 10; // num of frames to hold a beat
+	var beatDecayRate = 0.98;
 	var beatMinVol = 0.15; // a volume less than this is no beat
 	var beatLevelUp = 1.05;
 
