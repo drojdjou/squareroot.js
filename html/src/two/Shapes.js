84f163d93b077c9604e5d1e6a3d263997f04dedf
bartekd <bartek@everyday3d.com>
2015-02-21 00:24:21 -0800

    Starting migration to ES6 modules [2]

---
+++ html/src/two/Shapes.js
@@ -0,0 +1,31 @@
+SQR.Shapes = {
+
+	circle: function(radius, color) {
+		return function(ctx) {
+			ctx.beginPath();
+			ctx.fillStyle = color;
+			ctx.arc(0, 0, radius, 0, Math.PI * 2);
+			ctx.fill();
+		}
+	},
+
+	triangleEq: function(radius, color) {
+		return function(ctx) {
+			ctx.fillStyle = color;
+			ctx.beginPath();
+			var r = radius;
+			ctx.moveTo(Math.cos(30/180*Math.PI) * r, Math.sin(30/180*Math.PI) * r);
+			ctx.lineTo(Math.cos(150/180*Math.PI) * r, Math.sin(150/180*Math.PI) * r);
+			ctx.lineTo(Math.cos(Math.PI/-2) * r, Math.sin(Math.PI/-2) * r);
+			ctx.fill();
+		}
+	},
+
+	quad: function(width, height, color) {
+		return function(ctx) {
+			ctx.fillStyle = color;
+			ctx.fillRect(width*-0.5, height*-0.5, width, height);
+		}
+	}
+
+}
