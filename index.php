95224f805f46ff3670c0cb155818c312cb1fb2a0
bartekd <bartek@everyday3d.com>
2015-10-27 22:04:16 -0700

    Adding GA tracking to index.php

--- index.php
+++ index.php
@@ -95,6 +95,17 @@
 </section>
 
 <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-68827452-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<script>
 if(location.host.indexOf('local') > -1 || location.host.indexOf('192.168') > -1) {
 	document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
 }
