142856457c8501ce5d65ca83a9e1224ba34b56ec
Bartek <bartek@everyday3d.com>
2022-10-20 17:05:08 -0700

    Just playing with dust

--- html/lab/gl_dust/dust.glsl
+++ html/lab/gl_dust/dust.glsl
@@ -4,6 +4,7 @@ attribute vec2 aPosition;
 attribute float aBirth;
 attribute float aSize;
 attribute vec2 aEnergy;
+attribute vec3 aColor;
 
 uniform float uTime;
 uniform mat4 uViewMatrix;
@@ -12,6 +13,8 @@ uniform mat4 uProjection;
 uniform vec2 uGravity;
 
 varying float vLife;
+varying vec3 vColor;
+
 
 const float timescale = 0.01;
 
@@ -22,6 +25,8 @@ void main() {
 	vec2 g = uGravity;
 	g.y += aSize * 0.01;
 	g *= l;
+
+	vColor = aColor;
 
 	vec2 p = aPosition - l * aEnergy - g * g;
 	gl_Position = uProjection * uViewMatrix * vec4(p, 0.0, 1.0);
@@ -37,10 +42,11 @@ uniform sampler2D uTexture;
 uniform float uTime;
 
 varying float vLife;
+varying vec3 vColor;
 
 void main() {
 	vec4 c = texture2D(uTexture, gl_PointCoord);
-	gl_FragColor = vec4(c.rgb, vLife * c.a * 0.5);
+	gl_FragColor = vec4(c.rgb * vColor, vLife * c.a * 0.5);
 	// gl_FragColor = vec4(1.0, 1.0, 1.0, uLife);
 }
 
